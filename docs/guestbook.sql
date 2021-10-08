df5ae49f6b5ef7e9e7eb405000cc44b5c5a56dd7
naeunkim1227 <kne0140@daum.net>
2021-10-08 15:41:05 +0900

    21.10.08

---
+++ docs/guestbook.sql
@@ -0,0 +1,17 @@
+-- guestbook
+desc guestbook;
+
+-- insert
+insert into gueskbook values (null, '크롱', '1234', '크롱' , now() );
+
+-- select
+select no,name, date_format(reg_date, '%Y/%m/%d %H:%i:%s'), message 
+from gueskbook 
+order by reg_date desc;
+
+select * from guestbook;
+
+-- delete
+delete from gueskbook where no = 2 and password = '1235';
+
+
